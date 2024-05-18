54310623d3601f706044458e065d61514458a5ba
vishal kaira <kairavishal37@gmail.com>
2024-05-18 15:37:11 +0530

    Completed basic setup for authentication url part by adding url for login and logout.

---
+++ finance_tracker/trackit/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('add_income/', views.add_income, name='add_income'),
+    # path('add_expense/', views.add_expense, name='add_expense'),
+    # path('dashboard/', views.dashboard, name='dashboard'),
+    # path('report/', views.report, name='report'),
+    # # Add additional URL patterns for your views as needed
+]
