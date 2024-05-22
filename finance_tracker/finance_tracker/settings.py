0ef8933664701dca440c0cebd368d9bcb6729997
vishal kaira <kairavishal37@gmail.com>
2024-05-23 00:12:42 +0530

    link problem solved.

--- finance_tracker/finance_tracker/settings.py
+++ finance_tracker/finance_tracker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n2z-nk0_awlvi&_snjjwjzkvm9s^@$f#8e7hnmcen^&rxv0$gk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
@@ -141,9 +141,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # This is optional but recommended for custom static files
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS=os.path.join(BASE_DIR,'static'),
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
